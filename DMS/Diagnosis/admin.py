10d9a8fc8c55434d3f69ca7137ee509f1f955867
jinimp <linjinquan26@gmail.com>
2019-01-30 11:47:10 +0800

    修改朱博士诊断admin

--- DMS/Diagnosis/admin.py
+++ DMS/Diagnosis/admin.py
@@ -13,10 +13,17 @@ class DiagnosisAdmin(admin.ModelAdmin):
     #     return obj.image_info_diagnosis.pathology
 
     # 在文章列表页面显示的字段, 不是详情里面的字段
-    list_display = ['id', 'pathology', 'diagnosis_label_lastest', 'create_time', 'update_time']
+    list_display = ['id', 'pathology', 'diagnosis_label_lastest', 'his_diagnosis_label1',
+                    'his_diagnosis_label2', 'create_time', 'update_time']
 
     # 设置哪些字段可以点击进入编辑界面
     list_display_links = ('id', 'pathology')
 
     # 每页显示10条记录
     list_per_page = 10
+
+    # 按最新创建的时间排序. ordering设置默认排序字段，负号表示降序排序
+    ordering = ('-update_time',)
+
+    # 搜索栏
+    search_fields = ['pathology']
