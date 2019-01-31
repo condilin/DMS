ebfbd7d67374de451ff8d036917cc660244750c7
jinimp <linjinquan26@gmail.com>
2019-01-31 10:34:42 +0800

    修改片源匹配规则

--- DMS/Image/views.py
+++ DMS/Image/views.py
@@ -236,7 +236,7 @@ class UpdateDataBase(APIView):
             waveplate_source = '南方医院或华银'
 
         else:
-            waveplate_source = None
+            waveplate_source = 'unknown'
 
         return waveplate_source
 
