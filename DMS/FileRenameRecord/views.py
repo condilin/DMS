0de37203a5da46ede4d64ce539ef338d74670b15
jinimp <linjinquan26@gmail.com>
2019-02-16 20:16:43 +0800

    撤消修改文件上传功能bug,不需要添加header=None,因为指定一定要有表头

--- DMS/FileRenameRecord/views.py
+++ DMS/FileRenameRecord/views.py
@@ -48,10 +48,10 @@ class UploadFile(APIView):
         # ---------- 读取上传文件数据 ---------- #
         # excel格式
         if suffix_name in ['.xls', '.xlsx']:
-            data = pd.read_excel(upload_file_rename, header=None)
+            data = pd.read_excel(upload_file_rename)
         # csv格式
         else:
-            data = pd.read_csv(upload_file_rename, header=None)
+            data = pd.read_csv(upload_file_rename)
 
         # ---------- 删除上传文件数据 ---------- #
         os.remove(upload_file_rename)
